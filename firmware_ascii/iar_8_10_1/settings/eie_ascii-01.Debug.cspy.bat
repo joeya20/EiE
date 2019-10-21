329de3ab3c8feee2663d1550d23a9b38aa81ced1
joeya20 <56373482+joeya20@users.noreply.github.com>
2019-10-21 13:48:52 -0600

    ASCII DEV BOARD FIMRWARE

--- firmware_ascii/iar_8_10_1/settings/eie_ascii-01.Debug.cspy.bat
+++ firmware_ascii/iar_8_10_1/settings/eie_ascii-01.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\razor_sam3u2\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.general.xcl" --backend -f "C:\razor_sam3u2\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Github\EiE\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.general.xcl" --backend -f "C:\Github\EiE\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\razor_sam3u2\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\razor_sam3u2\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Github\EiE\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Github\EiE\firmware_ascii\iar_8_10_1\settings\eie_ascii-01.Debug.driver.xcl" 
 
 @echo off 
 :end
